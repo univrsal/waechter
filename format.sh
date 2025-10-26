92f5b6bed733b97a6f3c2109d3cad180e30735da
univrsal <uni@vrsal.xyz>
2025-10-26 15:24:28 +0100

    Misc: Remove format script

--- format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find ./tests -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
-find ./daemon -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
-find ./shim -iname *.h* -o -iname *.c* | xargs clang-format -i -verbose
-
