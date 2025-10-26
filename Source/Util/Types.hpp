21b69505a52480b5ba90d2a54ea7064853e65414
univrsal <uni@vrsal.xyz>
2025-10-26 14:53:38 +0100

    Types: Add traffic tracking types

--- Source/Util/Types.hpp
+++ Source/Util/Types.hpp
@@ -5,6 +5,15 @@
 #pragma once
 #include <sys/types.h>
 #include <cstdint>
+#include <chrono>
+
+#define WKiB *1024
+#define WMiB *1024 WKiB
+#define WGiB *1024 WMiB
 
 using WProcessId = pid_t;
 using WSocketCookie = uint64_t;
+using WMsec = int64_t;
+using WBytes = uint64_t;
+using WBytesPerSecond = double;
+using WUsec = std::chrono::microseconds;
