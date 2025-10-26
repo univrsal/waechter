102bdabb1ef048d9efeaf658c292188b9b5e9572
univrsal <uni@vrsal.xyz>
2025-10-26 14:54:18 +0100

    IPAddress: Reformat

--- Source/Util/IPAddress.hpp
+++ Source/Util/IPAddress.hpp
@@ -15,46 +15,43 @@ namespace EIPFamily
 		IPv4 = 4,
 		IPv6 = 6
 	};
-}
+} // namespace EIPFamily
 
 namespace EProtocol
 {
 	enum Type : uint8_t
 	{
-		TCP   = 6,
-		UDP   = 17,
-		ICMP  = 1,
-		ICMPv6= 58
+		TCP = 6,
+		UDP = 17,
+		ICMP = 1,
+		ICMPv6 = 58
 	};
-}
+} // namespace EProtocol
 
 struct WIPAddress
 {
 	// IPv4: first 4 bytes used; IPv6: all 16 bytes used.
 	std::array<uint8_t, 16> Bytes;
-	EIPFamily::Type Family;
+	EIPFamily::Type         Family;
 
 	[[nodiscard]] std::string to_string() const
 	{
 		if (Family == EIPFamily::IPv4)
 		{
-			return std::to_string(Bytes[0]) + "." +
-				   std::to_string(Bytes[1]) + "." +
-				   std::to_string(Bytes[2]) + "." +
-				   std::to_string(Bytes[3]);
+			return std::to_string(Bytes[0]) + "." + std::to_string(Bytes[1]) + "." + std::to_string(Bytes[2]) + "." + std::to_string(Bytes[3]);
 		}
 
 		char buffer[40];
 		snprintf(buffer, sizeof(buffer),
-				 "%x:%x:%x:%x:%x:%x:%x:%x",
-				 (Bytes[0] << 8) | Bytes[1],
-				 (Bytes[2] << 8) | Bytes[3],
-				 (Bytes[4] << 8) | Bytes[5],
-				 (Bytes[6] << 8) | Bytes[7],
-				 (Bytes[8] << 8) | Bytes[9],
-				 (Bytes[10] << 8) | Bytes[11],
-				 (Bytes[12] << 8) | Bytes[13],
-				 (Bytes[14] << 8) | Bytes[15]);
+			"%x:%x:%x:%x:%x:%x:%x:%x",
+			(Bytes[0] << 8) | Bytes[1],
+			(Bytes[2] << 8) | Bytes[3],
+			(Bytes[4] << 8) | Bytes[5],
+			(Bytes[6] << 8) | Bytes[7],
+			(Bytes[8] << 8) | Bytes[9],
+			(Bytes[10] << 8) | Bytes[11],
+			(Bytes[12] << 8) | Bytes[13],
+			(Bytes[14] << 8) | Bytes[15]);
 		return { buffer };
 	}
 
@@ -83,7 +80,7 @@ struct WIPAddress
 struct WEndpoint
 {
 	WIPAddress Address{};
-	uint16_t    Port{}; // host byte order
+	uint16_t   Port{}; // host byte order
 
 	[[nodiscard]] std::string to_string() const
 	{
