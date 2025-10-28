f98544abdf30fbe36668bd0827df6dc1cea52a72
univrsal <uni@vrsal.xyz>
2025-10-28 21:10:59 +0100

    DaemonClient: No move for shared pointer

--- Source/Daemon/Communication/DaemonClient.hpp
+++ Source/Daemon/Communication/DaemonClient.hpp
@@ -23,7 +23,7 @@ class WDaemonClient
 
 public:
 	WDaemonClient(std::shared_ptr<WClientSocket> CS, WDaemonSocket* PS)
-		: ClientSocket(std::move(CS))
+		: ClientSocket(CS)
 		, ParentSocket(PS)
 	{
 	}
