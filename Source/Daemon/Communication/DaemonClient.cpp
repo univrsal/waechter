38ab5015a0373a2ceb751bc88ca63656cf5ffea9
univrsal <uni@vrsal.xyz>
2025-10-31 17:48:49 +0100

    DaemonClient: Idle if nothing to read

--- Source/Daemon/Communication/DaemonClient.cpp
+++ Source/Daemon/Communication/DaemonClient.cpp
@@ -8,12 +8,19 @@
 void WDaemonClient::ListenThreadFunction()
 {
 	WBuffer Buf{};
+	bool    bDataToRead{};
 	while (Running)
 	{
-		if (!ClientSocket->Receive(Buf))
+		if (!ClientSocket->Receive(Buf, &bDataToRead))
 		{
 			Running = false;
 			break;
+		}
+
+		if (!bDataToRead)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(50));
+			continue;
 		}
 
 		auto Type = ReadMessageTypeFromBuffer(Buf);
