1ebcfc903bf2cfebc635f5f25fc96b0204e3364a
univrsal <uni@vrsal.xyz>
2025-11-02 10:33:34 +0100

    Util: Remove unused json stuff

--- Source/Util/Json.hpp
+++ Source/Util/Json.hpp
@@ -8,27 +8,3 @@
 using WJson = json11::Json;
 using WJsonValue = json11::JsonValue;
 using EJsonParse = json11::JsonParse;
-
-// Json consts
-#define wJC static constexpr char const*
-
-wJC JSON_KEY_BINARY_PATH = "bpath";
-wJC JSON_KEY_BINARY_NAME = "bname";
-wJC JSON_KEY_UPLOAD = "ul";
-wJC JSON_KEY_DOWNLOAD = "dl";
-wJC JSON_KEY_PID = "pid";
-wJC JSON_KEY_CMDLINE = "cmdline";
-wJC JSON_KEY_SOCKET_COOKIE = "sc";
-wJC JSON_KEY_SOCKETS = "socks";
-wJC JSON_KEY_PROCESSES = "procs";
-wJC JSON_KEY_APPS = "apps";
-wJC JSON_KEY_HOSTNAME = "host";
-wJC JSON_KEY_ID = "id";
-wJC JSON_KEY_TYPE = "type";
-
-enum EJsonMsgType
-{
-	MT_TrafficData,
-	MT_NodeRemoved,
-	MT_NodeAdded
-};
