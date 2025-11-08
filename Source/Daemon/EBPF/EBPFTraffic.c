9be824f9f02862d1cd37fef04a4e39535bcde250
univrsal <uni@vrsal.xyz>
2025-11-08 12:02:18 +0100

    Ebpf: Move egress/ingress programs to separate file

---
+++ Source/Daemon/EBPF/EBPFTraffic.c
@@ -0,0 +1,75 @@
+//
+// Created by usr on 08/11/2025.
+//
+
+// Ingress/Egress Traffic measuring and shaping
+
+#include "EBPFInternal.h"
+
+// cgroup_skb ingress: capture incoming packet information
+SEC("cgroup_skb/ingress")
+int cgskb_ingress(struct __sk_buff* Skb)
+{
+	__u64                Cookie = bpf_get_socket_cookie(Skb);
+	struct WSocketEvent* SocketEvent = MakeSocketEvent(Cookie, NE_Traffic);
+
+	if (SocketEvent)
+	{
+		__builtin_memset(&SocketEvent->Data.TrafficEventData, 0, sizeof(SocketEvent->Data.TrafficEventData));
+		struct WSocketTrafficEventData* TrafficData = &SocketEvent->Data.TrafficEventData;
+		TrafficData->Direction = PD_Incoming;
+		TrafficData->Bytes = (__u64)Skb->len;
+		TrafficData->Timestamp = bpf_ktime_get_ns();
+
+		__u32 Slen = Skb->len;
+		__u32 Len = PACKET_HEADER_SIZE;
+
+		if (Slen < PACKET_HEADER_SIZE)
+		{
+			Len = Slen;
+		}
+
+		if (Len > 0)
+		{
+			bpf_skb_load_bytes(Skb, 0, TrafficData->RawData, Len);
+		}
+
+		bpf_ringbuf_submit(SocketEvent, 0);
+	}
+
+	return SK_PASS;
+}
+
+// cgroup_skb egress: capture outgoing packet information
+SEC("cgroup_skb/egress")
+int cgskb_egress(struct __sk_buff* Skb)
+{
+	__u64 Cookie = bpf_get_socket_cookie(Skb);
+
+	struct WSocketEvent* SocketEvent = MakeSocketEvent(Cookie, NE_Traffic);
+	if (SocketEvent)
+	{
+		__builtin_memset(&SocketEvent->Data.TrafficEventData, 0, sizeof(SocketEvent->Data.TrafficEventData));
+		struct WSocketTrafficEventData* TrafficData = &SocketEvent->Data.TrafficEventData;
+		TrafficData->Direction = PD_Outgoing;
+		TrafficData->Bytes = (__u64)Skb->len;
+		TrafficData->Timestamp = bpf_ktime_get_ns();
+
+		__u32 Slen = Skb->len;
+		__u32 Len = PACKET_HEADER_SIZE;
+
+		if (Slen < PACKET_HEADER_SIZE)
+		{
+			Len = Slen;
+		}
+
+		if (Len > 0)
+		{
+			bpf_skb_load_bytes(Skb, 0, TrafficData->RawData, Len);
+		}
+
+		bpf_ringbuf_submit(SocketEvent, 0);
+	}
+
+	return SK_PASS;
+}
