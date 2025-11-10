43541a8d439a64faf43e043cc9da0a1baeab6904
univrsal <uni@vrsal.xyz>
2025-11-10 18:03:12 +0100

    Test scripts: Bind udp socket

--- Meta/Test scripts/UdpSender.py
+++ Meta/Test scripts/UdpSender.py
@@ -8,6 +8,9 @@ MESSAGE = b"0" * 10  # 1 KB of data
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+sock.bind(("0.0.0.0", 5005))
+sock.connect((UDP_IP, 5005))
+
 for i in range(0, 5000):
     print(f"sent {sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))}")
     time.sleep(0.7)  # 10 ms interval for ~1kb/s
