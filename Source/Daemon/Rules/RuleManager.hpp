f3ae2a0e7dee45b9e0dabc766f4cf27d99d2d621
univrsal <uni@vrsal.xyz>
2025-11-21 21:16:29 +0100

    RuleManager: Fix some compile errors

--- Source/Daemon/Rules/RuleManager.hpp
+++ Source/Daemon/Rules/RuleManager.hpp
@@ -8,26 +8,28 @@
 
 #include <unordered_map>
 #include <mutex>
+#include <memory>
 
 #include "Singleton.hpp"
 #include "Data/RuleUpdate.hpp"
 #include "Data/TrafficItem.hpp"
 
+enum WSocketRuleLevel : uint8_t
+{
+	SRL_None = 0,
+	SRL_Application = 1,
+	SRL_Process = 2,
+	SRL_Socket = 3,
+};
+
+struct WSocketRulesEntry
+{
+	WSocketRules     Rules;
+	WSocketRuleLevel Level{ SRL_None };
+};
+
 class WRuleManager : public TSingleton<WRuleManager>
 {
-	enum WSocketRuleLevel : uint8_t
-	{
-		SRL_None = 0,
-		SRL_Application = 1,
-		SRL_Process = 2,
-		SRL_Socket = 3,
-	};
-
-	struct WSocketRulesEntry
-	{
-		WSocketRules     Rules;
-		WSocketRuleLevel Level{ SRL_None };
-	};
 
 	std::mutex Mutex;
 
@@ -35,7 +37,6 @@ class WRuleManager : public TSingleton<WRuleManager>
 
 	void UpdateLocalRuleCache(WRuleUpdate const& Update);
 	void SyncWithEbpfMap();
-
 	void HandleSocketRuleUpdate(
 		std::shared_ptr<ITrafficItem> Item, WRuleUpdate const& Update, WSocketRuleLevel Level = SRL_Socket);
 	void HandleProcessRuleUpdate(
