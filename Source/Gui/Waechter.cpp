72d842fe14b308c232ae671079639085e50cc17e
univrsal <uni@vrsal.xyz>
2026-02-03 19:14:10 +0100

    Gui: Remove command line window

--- Source/Gui/Waechter.cpp
+++ Source/Gui/Waechter.cpp
@@ -5,8 +5,22 @@
 
 #include "Windows/GlfwWindow.hpp"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#ifdef _WIN32
+// When building as a Windows GUI application, use WinMain as the entry point
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	(void)hInstance;
+	(void)hPrevInstance;
+	(void)lpCmdLine;
+	(void)nCmdShow;
+#else
 int main()
 {
+#endif
 
 #ifndef _WIN32
 	// Just to suppress some warnings when opening URLs
