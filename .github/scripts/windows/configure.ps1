0ee4ab6f2a7d04bf33e8325ded54bed037ee76a9
univrsal <uni@vrsal.xyz>
2026-06-11 21:03:54 +0200

    CI: Configure with VS 2026

--- .github/scripts/windows/configure.ps1
+++ .github/scripts/windows/configure.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = "Stop"
 
 Write-Host "Configuring CMake..."
-cmake -S . -B build -G "Visual Studio 17 2022" -A x64 `
+cmake -S . -B build -G "Visual Studio 18 2026" -A x64 `
   -DCMAKE_BUILD_TYPE=Release `
   -DCMAKE_TOOLCHAIN_FILE="vcpkg/scripts/buildsystems/vcpkg.cmake" `
   -DWAECHTER_BUILD_DAEMON=OFF `
