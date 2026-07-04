ada58491eee6ec4222236094845bb3c46ea827cf
univrsal <uni@vrsal.xyz>
2026-07-04 20:51:42 +0200

    Assets: Add stats icon

--- Source/Gui/Icons/IconAtlas.cpp
+++ Source/Gui/Icons/IconAtlas.cpp
@@ -21,11 +21,12 @@ std::unordered_map<std::string, std::pair<ImVec2, ImVec2>> const ICON_ATLAS_UV =
 	{ "process", std::make_pair(ImVec2(0.136719f, 0.136719f), ImVec2(0.261719f, 0.261719f)) },
 	{ "proxy", std::make_pair(ImVec2(0.269531f, 0.136719f), ImVec2(0.394531f, 0.261719f)) },
 	{ "server", std::make_pair(ImVec2(0.402344f, 0.136719f), ImVec2(0.527344f, 0.261719f)) },
-	{ "tor", std::make_pair(ImVec2(0.535156f, 0.136719f), ImVec2(0.660156f, 0.261719f)) },
-	{ "uploadallow", std::make_pair(ImVec2(0.667969f, 0.136719f), ImVec2(0.792969f, 0.261719f)) },
-	{ "uploadblock", std::make_pair(ImVec2(0.800781f, 0.136719f), ImVec2(0.925781f, 0.261719f)) },
-	{ "uploadlimit", std::make_pair(ImVec2(0.003906f, 0.269531f), ImVec2(0.128906f, 0.394531f)) },
-	{ "vpn", std::make_pair(ImVec2(0.136719f, 0.269531f), ImVec2(0.261719f, 0.394531f)) },
+	{ "stats", std::make_pair(ImVec2(0.535156f, 0.136719f), ImVec2(0.660156f, 0.261719f)) },
+	{ "tor", std::make_pair(ImVec2(0.667969f, 0.136719f), ImVec2(0.792969f, 0.261719f)) },
+	{ "uploadallow", std::make_pair(ImVec2(0.800781f, 0.136719f), ImVec2(0.925781f, 0.261719f)) },
+	{ "uploadblock", std::make_pair(ImVec2(0.003906f, 0.269531f), ImVec2(0.128906f, 0.394531f)) },
+	{ "uploadlimit", std::make_pair(ImVec2(0.136719f, 0.269531f), ImVec2(0.261719f, 0.394531f)) },
+	{ "vpn", std::make_pair(ImVec2(0.269531f, 0.269531f), ImVec2(0.394531f, 0.394531f)) },
 };
 
 void WIconAtlas::Load()
