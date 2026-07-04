be9d59dd46c5776f3d27219e5cb1657bc9eefc62
univrsal <uni@vrsal.xyz>
2026-07-04 13:50:03 +0200

    Migrations: Re-create table to add new column

--- Meta/Database/Migrations/0004_add_asn.sqlite.sql
+++ Meta/Database/Migrations/0004_add_asn.sqlite.sql
@@ -5,5 +5,18 @@ CREATE TABLE IF NOT EXISTS "Asn" (
 	"Country"	TEXT NOT NULL,
 	PRIMARY KEY("ID")
 );
-ALTER TABLE "Host" ADD COLUMN "AsnID" INTEGER;
-ALTER TABLE "Host" ADD FOREIGN KEY("AsnID") REFERENCES "Asn"("ID");
+PRAGMA foreign_keys = OFF;
+CREATE TABLE "Host_new" (
+	"ID"	INTEGER,
+	"Family"	INTEGER NOT NULL CHECK("Family" IN (4, 6)),
+	"IPAddress"	BLOB NOT NULL,
+	"AsnID"	INTEGER,
+	PRIMARY KEY("ID"),
+	FOREIGN KEY("AsnID") REFERENCES "Asn"("ID")
+);
+INSERT INTO "Host_new" ("ID", "Family", "IPAddress", "AsnID")
+SELECT "ID", "Family", "IPAddress", NULL
+FROM "Host";
+DROP TABLE "Host";
+ALTER TABLE "Host_new" RENAME TO "Host";
+PRAGMA foreign_keys = ON;
